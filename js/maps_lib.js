e61a920e66e00228c05bc6be7f66fdf8bf2f4061
Josh Kalov <jkalov@gmail.com>
2018-02-23 17:59:40 -0600

    map styles + helper text

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -52,6 +52,22 @@
         };
         this.geocoder = new google.maps.Geocoder();
         this.map = new google.maps.Map($("#map_canvas")[0], this.myOptions);
+        var styles = [
+            {
+                stylers: [
+                { saturation: -70 }
+                ]
+            },{
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [
+                { lightness: 100 },
+                { visibility: "simplified" }
+                ]
+            }
+        ];  
+        this.map.setOptions({styles: styles});
+          
         
         // maintains map centerpoint for responsive design
         google.maps.event.addDomListener(self.map, 'idle', function () {
@@ -97,8 +113,8 @@
                 where: whereClause
             },
             styleId: 2,
-            templateId: 2,
-            suppressInfoWindows:true
+            templateId: 3,
+            suppressInfoWindows:false
         });
         self.fusionTable = self.searchrecords;
         self.searchrecords.setMap(map);
@@ -124,19 +140,21 @@
                     $.address.parameter('radius', encodeURIComponent(self.searchRadius));
                     map.setCenter(self.currentPinpoint);
                     // set zoom level based on search radius
-                    if (self.searchRadius >= 1610000) map.setZoom(4); // 1,000 miles
-                    else if (self.searchRadius >= 805000) map.setZoom(5); // 500 miles
-                    else if (self.searchRadius >= 402500) map.setZoom(6); // 250 miles
-                    else if (self.searchRadius >= 161000) map.setZoom(7); // 100 miles
-                    else if (self.searchRadius >= 80500) map.setZoom(8); // 100 miles
-                    else if (self.searchRadius >= 40250) map.setZoom(9); // 100 miles
-                    else if (self.searchRadius >= 16100) map.setZoom(11); // 10 miles
-                    else if (self.searchRadius >= 8050) map.setZoom(12); // 5 miles
-                    else if (self.searchRadius >= 3220) map.setZoom(13); // 2 miles
-                    else if (self.searchRadius >= 1610) map.setZoom(14); // 1 mile
-                    else if (self.searchRadius >= 805) map.setZoom(15); // 1/2 mile
-                    else if (self.searchRadius >= 400) map.setZoom(16); // 1/4 mile
-                    else self.map.setZoom(17);
+                    // if (self.searchRadius >= 1610000) map.setZoom(4); // 1,000 miles
+                    // else if (self.searchRadius >= 805000) map.setZoom(5); // 500 miles
+                    // else if (self.searchRadius >= 402500) map.setZoom(6); // 250 miles
+                    // else if (self.searchRadius >= 161000) map.setZoom(7); // 100 miles
+                    // else if (self.searchRadius >= 80500) map.setZoom(8); // 100 miles
+                    // else if (self.searchRadius >= 40250) map.setZoom(9); // 100 miles
+                    // else if (self.searchRadius >= 16100) map.setZoom(11); // 10 miles
+                    // else if (self.searchRadius >= 8050) map.setZoom(12); // 5 miles
+                    // else if (self.searchRadius >= 3220) map.setZoom(13); // 2 miles
+                    // else if (self.searchRadius >= 1610) map.setZoom(14); // 1 mile
+                    // else if (self.searchRadius >= 805) map.setZoom(15); // 1/2 mile
+                    // else if (self.searchRadius >= 400) map.setZoom(16); // 1/4 mile
+                    // else self.map.setZoom(17);
+
+                    map.setZoom(self.defaultZoom);
 
                     if (self.addrMarkerImage != '') {
                         self.addrMarker = new google.maps.Marker({
